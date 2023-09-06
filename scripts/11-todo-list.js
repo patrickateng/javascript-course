78d766320d6ce3f6bed8cddde8cb126428ee7219
patrickateng <99479428+patrickateng@users.noreply.github.com>
2023-09-06 21:31:11 +0300

    Javascript-course Lesson 11

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,15 @@
+const todoList = []
+
+function addTodo() {
+
+  const inputElement = document.querySelector('.js-name-input')
+
+  const name = inputElement.value
+
+  todoList.push(name)
+
+  console.log(todoList)
+
+  inputElement.value = ''
+
+}
